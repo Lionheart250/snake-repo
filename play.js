3dbddf5aa0a34875d1f06c07df04067900448685
vagrant <jacksonlionheart@gmail.com>
2023-07-05 19:33:18 +0000

    add

--- play.js
+++ play.js
@@ -1,16 +1,33 @@
 const net = require("net");
-const connect = require("./client");// connect function
-// establishes a connection with the game server
-const conn = connect();
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+const connect = require("./client");
 
-  // event handler for data
-  conn.on('data', (data) => {
-    console.log("server says:", data);
-  });
-  return conn;
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
 
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  // Other key handling logic here
+};
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
+
+// Interpret incoming data as text
+conn.setEncoding("utf8");
+
+// Event handler for data
+conn.on('data', (data) => {
+  console.log("Server says:", data);
+});
+
+// Call the setupInput function
+setupInput();
